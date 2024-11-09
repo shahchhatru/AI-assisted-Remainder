e829e5545a1ad31a11617cd4e8b6b6818515d4e7
Ashwini <mandal.ashwini1412@gmail.com>
2024-11-09 21:28:25 +0545

    update main.py to use new LLM function

--- ai-backend/main.py
+++ ai-backend/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import HTTPException
-from llm_inference.src.llm_inference.inference import html_parser
+from llm_inference.src.llm_inference.inference import html_parser, required_content_bool
 
 app = FastAPI()
 
@@ -15,10 +15,11 @@ app.add_middleware(
 )
 
 @app.post("/run_prompt")
-async def api_run_prompt(content: str):
+async def api_run_prompt(content: str, user_req: str):
     try:
-        response = await html_parser(content)
-        return {"response": response}
+        parsed_response = await html_parser(content)
+        required_content = await required_content_bool(parsed_response, user_req)
+        return {"response": required_content}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
