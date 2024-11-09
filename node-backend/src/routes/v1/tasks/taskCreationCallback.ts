7c15fa7d65bb91cdb6dca518b57c4c0668bea9f8
sauravkhanal <sauravkhanal635@gmail.com>
2024-11-10 02:49:26 +0545

    sample data

--- node-backend/src/routes/v1/tasks/taskCreationCallback.ts
+++ node-backend/src/routes/v1/tasks/taskCreationCallback.ts
@@ -19,8 +19,25 @@ export function taskCreationCallback(taskDetails: ITasks) {
      * 
      */
     console.log(`A task has been created with following details: `, taskDetails)
+
+    /**
+     * sample 
+     * A task has been created with following details:  {
+  uid: 'auth0|672f5297172b35246cf6bdcb',
+  url: 'daraz.com.np',
+  queryTerms: 'olaol',
+  hourInterval: 1,
+  daysInterval: 1,
+  time: '2024-11-09T20:53:59.866Z',
+  notificationType: 'Hourly',
+  _id: new ObjectId('672fcde0e94794f62f09267e'),
+  createdAt: 2024-11-09T21:02:24.335Z,
+  updatedAt: 2024-11-09T21:02:24.335Z,
+  __v: 0
+}
+     */
 }
 
-function webScrapingFunction () {
+function webScrapingFunction() {
     // 
 }
